fe2fc0824dea5df844ce3f1376b9b62bd9183b23
Archawatip <Archawatip_seeneha@kkumail.com>
2021-06-26 17:33:48 +0700

    Backend 1.1
    Query product
    +sortBy
    +order
    +limit
    Product Model
    +Sold

--- controllers/product_controller.js
+++ controllers/product_controller.js
@@ -115,3 +115,25 @@ exports.remove = (req,res) =>{
         })
     })
 }
+
+// ขาย / มีของ
+
+exports.list = (req,res) => {
+    let order = req.query.order ? req.query.order : 'asc'
+    let sortBy = req.query.sortBy ? req.query.sortBy : '_id'
+    let limit = req.query.limit ? parseInt(req.query.limit) : 6
+
+    Product.find()
+        .select('-photo')
+        .populate('category')
+        .sort([[sortBy,order]])
+        .limit(limit)
+        .exec((err,products) => {
+            if (err){
+                return res.status(400).json({
+                    error: 'ไม่พบสินค้านี้ในระบบ'
+                })
+            }
+            res.send(products)
+        })
+}
--- routes/product_route.js
+++ routes/product_route.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const router = express.Router()
 
-const { create ,productById,read,remove,update} = require("../controllers/product_controller")
+const { create ,productById,read,remove,update,list} = require("../controllers/product_controller")
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user_controller");
+
+
+router.get('/products',list)
 
 router.get('/product/read/:productId',read)
 router.post("/product/create/:userId",requireSignin,isAdmin,isAuth,create);
 router.delete('/product/delete/:productId/:userId',requireSignin,isAdmin,isAuth,remove)
-    router.put('/product/update/:productId/:userId',requireSignin,isAdmin,isAuth,update)
+router.put('/product/update/:productId/:userId',requireSignin,isAdmin,isAuth,update)
 
 
 router.param('userId', userById)
