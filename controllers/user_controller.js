122432e66ef7c56431af1a498f104fc58f1b27b8
Archawatip <Archawatip_seeneha@kkumail.com>
2021-06-26 19:49:17 +0700

    BACKEND 1.6
    +READ USER PROFILE
    +UPDATE USER PROFILE

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,7 +1,7 @@
-const User_controller = require('../models/user_model')
+const User = require('../models/user_model')
 
 exports.userById = (req,res,next,id) => {
-    User_controller.findById(id).exec((err, user)=>{
+    User.findById(id).exec((err, user)=>{
         if (err || !user){
             return res.status(400).json({
                 error: 'ไม่พบผู้ใช้นี้ในระบบ'
@@ -10,4 +10,26 @@ exports.userById = (req,res,next,id) => {
         req.profile = user;
         next();
     })
+}
+
+exports.read = (req,res) => {
+    req.profile.hashed_password = undefined
+    req.profile.salt = undefined
+    return res.json(req.profile);
+}
+
+exports.update = (req,res) => {
+    User.findOneAndUpdate({_id:req.profile},
+        {$set: req.body},
+        {new: true},
+        (err,user) =>{
+        if (err){
+            return res.status(400).json({
+                error: 'คุณต้องเข้าสู่ระบบเพื่อดำเนินการต่อ'
+            })
+        }
+            user.hashed_password = undefined
+            user.salt = undefined
+            res.json(user)
+        })
 }
